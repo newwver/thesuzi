7823844e57efc2a04739b5666d10a90e69502a47
newwver <yezicho11@gmail.com>
2024-08-03 10:57:40 +0900

    origin commit

---
+++ assets/js/lib/trans-animation.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+	var animationDuration = 2500;
+
+	function animateText($element) {
+		var nextElement = getNextElement($element);
+
+		$element.parents('.change-text-wrap').animate({ width: '2px' }, 600, function () {
+			toggleVisibility($element, nextElement);
+			$(this).animate({ width: nextElement.width() + 10 }, 600, function () {
+				setTimeout(function () {
+					animateText(nextElement);
+				}, animationDuration);
+			});
+		});
+	}
+
+	function toggleVisibility($current, $next) {
+		$current.removeClass('is-visible').addClass('is-hidden');
+		$next.removeClass('is-hidden').addClass('is-visible');
+	}
+
+	function getNextElement($current) {
+		return $current.is(':last-child') ? $current.parent().children().eq(0) : $current.next();
+	}
+
+	$('.change-text.clip .is-visible').each(function () {
+		var $this = $(this);
+		setTimeout(function () {
+			animateText($this);
+		}, animationDuration);
+	});
+});
---
+++ assets/js/main.js
@@ -0,0 +1,124 @@
+(function () {
+	'use strict';
+
+	const myMain = {
+		window: window,
+		document: document,
+		body: document.querySelector('body'),
+		html: document.querySelector('html'),
+
+		initialize: function () {
+			this.setupGlobals();
+			this.executeMethods();
+		},
+
+		setupGlobals: function () {
+			this.window = window;
+			this.document = document;
+			this.body = document.querySelector('body');
+			this.html = document.querySelector('html');
+		},
+
+		executeMethods: function () {
+			this.activateFeatherIcons();
+			this.initializeGoTop();
+			this.enableStickyHeader();
+			this.enableSmoothScroll();
+			this.blogActivation();
+			this.activateWOW();
+			this.activateAOS();
+			this.pageNavigation();
+		},
+
+		activateWOW: function () {
+			new WOW().init();
+		},
+
+		enableSmoothScroll: function () {
+			this.document.addEventListener(
+				'click',
+				function (event) {
+					const target = event.target;
+					if (target.classList.contains('smooth-animation')) {
+						event.preventDefault();
+						const targetElement = this.document.querySelector(target.getAttribute('href'));
+						this.window.scrollTo({
+							top: targetElement.offsetTop - 50,
+							behavior: 'smooth',
+						});
+					}
+				}.bind(this)
+			);
+		},
+
+		activateFeatherIcons: function () {
+			feather.replace();
+		},
+
+		initializeGoTop: function () {
+			const goTopButton = this.document.querySelector('.go-top');
+			this.window.addEventListener(
+				'scroll',
+				function () {
+					const baseline = this.window.scrollY || this.window.pageYOffset;
+					goTopButton.style.opacity = baseline > 100 ? '1' : '0';
+				}.bind(this)
+			);
+
+			goTopButton.addEventListener(
+				'click',
+				function () {
+					this.window.scrollTo({
+						top: 0,
+						behavior: 'smooth',
+					});
+					return false;
+				}.bind(this)
+			);
+		},
+
+		enableStickyHeader: function () {
+			this.window.addEventListener(
+				'scroll',
+				function () {
+					const headerSticky = this.document.querySelector('.header--sticky');
+					headerSticky.classList.toggle('sticky', this.window.scrollY > 30);
+				}.bind(this)
+			);
+		},
+
+		blogActivation: function () {
+			$('.blog-activation').slick({
+				infinite: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true,
+				arrows: true,
+				adaptiveHeight: true,
+				cssEase: 'linear',
+				prevArrow: '<button class="slide-arrow prev-arrow"><i class="feather-arrow-left"></i></button>',
+				nextArrow: '<button class="slide-arrow next-arrow"><i class="feather-arrow-right"></i></button>',
+			});
+		},
+
+		activateAOS: function () {
+			AOS.init();
+		},
+
+		pageNavigation: function () {
+			$('.page-navigation').onePageNav({
+				currentClass: 'current',
+				changeHash: true,
+				scrollSpeed: 500,
+				scrollThreshold: 0.2,
+				filter: ':not(.external)',
+				easing: 'swing',
+				scrollChange: function ($currentListItem) {
+					console.log(this);
+				}.bind(this),
+			});
+		},
+	};
+
+	myMain.initialize();
+})();
