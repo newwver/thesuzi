90d1f2c4eef38b0804504f6018f278ac2c1959dc
newwver <yezicho11@gmail.com>
2024-08-17 21:42:59 +0900

    sub에 home 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,14 +62,17 @@
       if (navLinks.length > 0) {
         navLinks.forEach((anchor) => {
           anchor.addEventListener('click', function (e) {
-            e.preventDefault();
             const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            if (targetElement) {
-              window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth',
-              });
+
+            if (targetId.startsWith('#')) {
+              e.preventDefault();
+              const targetElement = document.querySelector(targetId);
+              if (targetElement) {
+                window.scrollTo({
+                  top: targetElement.offsetTop,
+                  behavior: 'smooth',
+                });
+              }
             }
           });
         });
